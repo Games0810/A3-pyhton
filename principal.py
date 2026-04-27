c06ea68286c2624d20570922dc46ba2594e4bee7
Heitor <heitor.santos.ba@gmail.com>
2026-04-27 16:52:38 -0300

    Adionei o tratamento de erro na escolha do que fazer e ajetei a escrita de adicionar coisas novas na lista

--- principal.py
+++ principal.py
@@ -1,90 +1,123 @@
 import json
 from random import randint
 
-
 while True:
-    try:
-        escolha = int(input("""
+    print("""
     Bem vindo, escolha uma das coisas a seguir:
     1 - Ver lista
     2- Adicionar
     3- Excluir
     4- Editar
-    5- Sair
-        Resposta: """))
-    
-        with open("seleções.json", "r") as arq:
-            dados = json.load(arq)
-    
-        if escolha == 1:
+    5- Sair""")
+    try:
+        escolha=int(input("Resposta :"))
+
+    except:
+        print("Digite um número")
+        continue
+
+    if escolha not in (1,2,3,4,5):
+        print("Escolha um número na lista")
+
+    with open("seleções.json", "r") as arq:
+        dados = json.load(arq)
+
+    if escolha == 1:
+        print("-="*20)
+        for lista in dados["s"]:
+            print(f"ID: {lista['ID']}")
+            print(f"País: {lista['País']}")
+            print(f"Grupo: {lista['Grupo']}")
+            print(f"Confederação: {lista['Confederação']}")
+            print(f"Treinador: {lista['Treinador']}")
             print("-="*20)
-            for lista in dados["s"]:
-                print(f"ID: {lista['ID']}")
-                print(f"País: {lista['País']}")
-                print(f"Grupo: {lista['Grupo']}")
-                print(f"Confederação: {lista['Confederação']}")
-                print(f"Treinador: {lista['Treinador']}")
-                print("-="*20)
+
+    elif escolha == 2:
+        novo_id = ""
+        for i in range (10):
+            n = randint(1, 10)
+            novo_id += str(n)
+        novo_p = input("País: ").strip()
+
+        if novo_p == "":
+            print("Não pode digitar nada")
+        elif not novo_p.replace(" ", "").isalpha():
+            print("Digite somente letras")
+
+        novo_g = input("Grupo: ").strip()
+
+        if novo_g == "":
+            print("Não pode digitar nada")
+
+        novo_c = input("Confederação: ").strip()
+
+        if novo_c == "":
+            print("Não pode digitar nada")
+        elif not novo_c.replace(" ", "").isalpha():
+            print("Digite somente letras")
+
+        novo_t = input("Treinador: ").strip()
+
+        if novo_t == "":
+            print("Não pode digitar nada")
+        elif not novo_t.replace(" ", "").isalpha():
+            print("Digite somente letras")
+
+        nova_seleção = {"País": novo_p, "Grupo": novo_g, "Confederação": novo_c, "Treinador": novo_t, "ID": novo_id}
+        dados["s"].append(nova_seleção)
+
+        with open("seleções.json", "w") as arq:
+            json.dump(dados, arq, indent=4, ensure_ascii=False)
     
-        elif escolha == 2:
-            novo_id = ""
-            for i in range (10):
-                n = randint(1, 10)
-                novo_id += str(n)
-            novo_p = input("País: ")
-            novo_g = input("Grupo: ")
-            novo_c = input("Confederação: ")
-            novo_t = input("Treinador: ")
-    
-            nova_seleção = {"País": novo_p, "Grupo": novo_g, "Confederação": novo_c, "Treinador": novo_t, "ID": novo_id}
-            dados["s"].append(nova_seleção)
-    
-            with open("seleções.json", "w") as arq:
-                json.dump(dados, arq, indent=4, ensure_ascii=False)
+    elif escolha == 3:
+        remover = input("Nome do País: ")
+        for lista in dados["s"]:
+            if lista["País"] == remover:
+                dados["s"].remove(lista)
+                print("Removido")
+                break
         
-        elif escolha == 3:
-            remover = input("Nome do País: ")
-            for lista in dados["s"]:
-                if lista["País"] == remover:
-                    dados["s"].remove(lista)
-                    print("Removido")
-                    break
-            
-                else:
-                    print("Não encontrado")
-    
+            else:
+                print("Não encontrado")
+
+        with open("seleções.json", "w") as arq:
+            json.dump(dados, arq, indent=4, ensure_ascii=False)
+
+    elif escolha == 4:
+        edit = int(input("Qual elemento? \n 1- País \n 2 - Grupo \n 3 - Confederação \n 4 - Treinador "))
+        if edit == 1:
+            nome = input("Fale o nome do País: ")
+            for l in dados["s"]: 
+                if l["País"] == nome:
+                    novo_país = input("Digite o novo nome: ")
+                    l["País"] = novo_país
             with open("seleções.json", "w") as arq:
                 json.dump(dados, arq, indent=4, ensure_ascii=False)
-    
-        elif escolha == 4:
-            edit = int(input("Qual elemento? \n 1- País \n 2 - Grupo \n 3 - Confederação \n "))
-            if edit == 1:
-                nome = input("Fale o nome do País: ")
-                for l in dados["s"]: 
-                    if l["País"] == nome:
-                        novo_país = input("Digite o novo nome: ")
-                        l["País"] = novo_país
-                with open("seleções.json", "w") as arq:
-                    json.dump(dados, arq, indent=4, ensure_ascii=False)
-    
-            elif edit == 2:
-                nome = input("Fale o nome do País: ")
-                for l in dados["s"]: 
-                    if l["País"] == nome:
-                        novo_grupo = input("Digite o novo nome: ")
-                        l["Grupo"] = novo_grupo
-                with open("seleções.json", "w") as arq:
-                    json.dump(dados, arq, indent=4, ensure_ascii=False)
-            elif edit == 3:
-                nome = input("Fale o nome do País: ")
-                for l in dados["s"]: 
-                    if l["País"] == nome:
-                        novo_confederação = input("Digite o novo nome: ")
-                        l["Confederação"] = novo_confederação
-                with open("seleções.json", "w") as arq:
-                    json.dump(dados, arq, indent=4, ensure_ascii=False)
-    
-        elif escolha == 5:
-            break
-    except ValueError:
-        print("- CAIXAS VAZIAS NÃO SÃO ACEITAS -")
+
+        elif edit == 2:
+            nome = input("Fale o nome do País: ")
+            for l in dados["s"]: 
+                if l["País"] == nome:
+                    novo_grupo = input("Digite o novo nome: ")
+                    l["Grupo"] = novo_grupo
+            with open("seleções.json", "w") as arq:
+                json.dump(dados, arq, indent=4, ensure_ascii=False)
+        elif edit == 3:
+            nome = input("Fale o nome do País: ")
+            for l in dados["s"]: 
+                if l["País"] == nome:
+                    novo_confederação = input("Digite o novo nome: ")
+                    l["Confederação"] = novo_confederação
+            with open("seleções.json", "w") as arq:
+                json.dump(dados, arq, indent=4, ensure_ascii=False)
+        elif edit == 4:
+            nome = input("Fale o nome do País: ")
+            for l in dados["s"]: 
+                if l["País"] == nome:
+                    novo_treinador = input("Digite o novo nome: ")
+                    l["Treinador"] = novo_treinador
+            with open("seleções.json", "w") as arq:
+                json.dump(dados, arq, indent=4, ensure_ascii=False)
+
+    elif escolha == 5:
+        break
